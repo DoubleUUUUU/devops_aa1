f99184555c248a4dfbad39993caa487b17c5ce3e
) <100924402+DoubleUUUUU@users.noreply.github.com>
2025-03-24 14:40:00 +0100

    alles vorhanden ohne unused nochmal2

--- src/app/components/ui/CarList.tsx
+++ src/app/components/ui/CarList.tsx
@@ -10,7 +10,7 @@ export default function CarList({ cars }: Props) {
         <h1>CarList:</h1>
         <div className='grid grid-cols-3 md:grid-cols-4 gap-4'>
             {cars.map((car: Car) => (
-            <Card car={car}/>
+            <Card key={car.motor.serialNumber} car={car}/>
         ))}
         </div>
          
