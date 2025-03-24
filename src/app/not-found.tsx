08df62335549c3a2179f78005695822a0d9599bd
) <100924402+DoubleUUUUU@users.noreply.github.com>
2025-03-24 14:42:42 +0100

    unnötiges löschen 4

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
-type Props = {}
 
-export default function NotFound({}: Props) {
+export default function NotFound() {
   return (
     <div>We have a big problem</div>
   )
